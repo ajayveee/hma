3583a4ae08ddc79bb9b0772ee4e28bcdfad399c9
Ajay V <ajayveee@gmail.com>
2014-11-19 13:46:38 -0500

    added getter setter

--- hma-common/src/main/java/com/uncc/sem1/ssdi/hma/monitoring/domain/Target.java
+++ hma-common/src/main/java/com/uncc/sem1/ssdi/hma/monitoring/domain/Target.java
@@ -3,7 +3,8 @@ package com.uncc.sem1.ssdi.hma.monitoring.domain;
 import java.util.Date;
 
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
-@JsonIgnoreProperties(ignoreUnknown=true)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Target {
 	private int targetId;
 	private Date startDate;
@@ -11,33 +12,59 @@ public class Target {
 	private TargetType targetType;
 	private int completedPercentage;
 	private int userId;
+
 	public Date getStartDate() {
 		return startDate;
 	}
+
+	public int getTargetId() {
+		return targetId;
+	}
+
+	public void setTargetId(int targetId) {
+		this.targetId = targetId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
+
 	public Date getEndDate() {
 		return endDate;
 	}
+
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
 	}
+
 	public TargetType getTargetType() {
 		return targetType;
 	}
+
 	public void setTargetType(TargetType targetType) {
 		this.targetType = targetType;
 	}
+
 	public int getCompletedPercentage() {
 		return completedPercentage;
 	}
+
 	public void setCompletedPercentage(int completedPercentage) {
 		this.completedPercentage = completedPercentage;
 	}
+
 	public int getUser() {
 		return userId;
 	}
+
 	public void setUser(int user) {
 		this.userId = user;
 	}
